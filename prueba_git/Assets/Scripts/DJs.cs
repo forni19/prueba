9ea0416939805ffe506644f80148f39f5b7f58c8
forni19 <campus@iMac-de-Campus-5.local>
2015-11-13 17:05:57 +0100

    Inicio Proyecto
    proyecto vacío

---
+++ prueba_git/Assets/Scripts/DJs.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DJs : MonoBehaviour {
+	public AudioClip sonido1;
+	public AudioClip sonido2;
+	public AudioClip sonido3;
+
+	private AudioSource au;
+	// Use this for initialization
+	void Start () {
+		au = GetComponent<AudioSource> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			au.PlayOneShot(sonido1,1);
+		}
+		if (Input.GetKeyDown (KeyCode.D)) {
+			au.PlayOneShot(sonido2,1);
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			au.PlayOneShot(sonido3,1);
+		}
+	
+	}
+}
---
+++ prueba_git/Assets/Scripts/SonidoBalon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+public class SonidoBalon : MonoBehaviour {
+
+
+	private AudioSource sonido;
+
+
+	// Use this for initialization
+	void Start () {
+		sonido = GetComponent<AudioSource> ();
+	
+	}
+	void OnCollisionEnter2D(Collision2D objeto){
+		sonido.Play ();
+		Debug.Log ("play");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
